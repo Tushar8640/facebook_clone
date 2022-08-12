5febd92595ca7c3bfb8f64736b6d59a33c2f5aaa
Tushar@8640 <imtantushar44@gmail.com>
2022-08-12 17:23:46 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-white">
+   
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <img
+            src="https://placeimg.com/260/400/arch"
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
+          
+          <div>
+            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, autem
+        corporis porro tempore soluta laborum iure odit pariatur vero deleniti.
+        Similique sunt facilis debitis, possimus nesciunt eveniet ut rerum
+        beatae?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, autem
+        corporis porro tempore soluta laborum iure odit pariatur vero deleniti.
+        Similique sunt facilis debitis, possimus nesciunt eveniet ut rerum
+        beatae?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, autem
+        corporis porro tempore soluta laborum iure odit pariatur vero deleniti.
+        Similique sunt facilis debitis, possimus nesciunt eveniet ut rerum
+        beatae?
+      </p>
+      <button className="btn btn-primary">Hello</button>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, autem
+        corporis porro tempore soluta laborum iure odit pariatur vero deleniti.
+        Similique sunt facilis debitis, possimus nesciunt eveniet ut rerum
+        beatae?
+      </p>
     </div>
   );
 }
